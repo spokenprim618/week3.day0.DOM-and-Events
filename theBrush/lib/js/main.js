fb89f318fcc12667920bc5eadfb51e99c2afb56b
unknown <williamV1905@mhbx.org>
2021-08-04 21:28:31 -0400

    fixed

--- theBrush/lib/js/main.js
+++ theBrush/lib/js/main.js
@@ -1,14 +1,19 @@
-document.createElement("button");
+let Button= document.createElement("button");
+const rengar= document.querySelector('#rengar');
+const body=document.querySelector("body");
 const myButton=document.querySelector("button");
 const text=document.createTextNode("click me");
-const body=document.querySelector("body");
-body.appendChild("myButton");
-myButton.appendChild("text");
-const rengar= document.querySelector('#rengar');
+body.appendChild(Button);
+
+
+
+
+Button.appendChild(text);
+
 
  function onClick(){
-   document.getElementsByID("rengar").src="assets/rengar.png";
+   document.getElementById("rengar").src="assets/rengar.png";
     alert("you've been deleted");
 };
- myButton.addEventListener("click", onClick);
+Button.addEventListener("click", onClick);
  //problem is my button is equal to null
